4371af0c1e9014155afa8e1d1f14f923835de736
Kim Chanho <beroso20th@gmail.com>
2023-11-02 02:01:49 +0900

    feature : 박스오피스, kmdb검색 작성중

--- src/main/java/com/example/magichour/controller/HomeController.java
+++ src/main/java/com/example/magichour/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.magichour.controller;
 import com.example.magichour.entity.Member;
 import com.example.magichour.util.OkHttpUtils;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.extern.log4j.Log4j2;
@@ -31,38 +32,53 @@ public class HomeController {
 
     @GetMapping("/")
     public String Home(@SessionAttribute(name = "loginMember", required = false) Member member, Model model) {
+        JsonObject movieObject = new JsonObject();
+
         // 박스오피스
-        String bofJsonString = OkHttpUtils.get(BOXOFFICE_URL + BOXOFFICE_API_KEY);
+        String boxofficeResponse = OkHttpUtils.get(BOXOFFICE_URL + BOXOFFICE_API_KEY);
+
         JsonParser jsonParser = new JsonParser();
-        JsonObject jsonObject = (JsonObject) jsonParser.parse(bofJsonString);
-
-        JsonObject boxOfficeResult = jsonObject.getAsJsonObject("boxOfficeResult");
+        JsonObject boxofficeRespToJson = (JsonObject) jsonParser.parse(boxofficeResponse);
+        JsonObject boxOfficeResult = boxofficeRespToJson.getAsJsonObject("boxOfficeResult");
         JsonArray dailyBoxOfficeList = boxOfficeResult.getAsJsonArray("dailyBoxOfficeList");
+        log.info(dailyBoxOfficeList);
 
-        JsonObject finalData2 = null;
 
         for(int i=0; i<dailyBoxOfficeList.size(); i++) {
-            String movieNm = dailyBoxOfficeList.get(i).getAsJsonObject().get("movieNm").getAsString();
-            log.info(movieNm);
-            String movieInfo = OkHttpUtils.get(KMDB_URL + KMDB_API_KEY + "&title=" + movieNm);
+            JsonElement movieNm = dailyBoxOfficeList.get(i).getAsJsonObject().get("movieNm");
+            JsonElement audiAcc = dailyBoxOfficeList.get(i).getAsJsonObject().get("audiAcc");
+            JsonElement openDt = dailyBoxOfficeList.get(i).getAsJsonObject().get("openDt");
+            JsonElement rank = dailyBoxOfficeList.get(i).getAsJsonObject().get("rank");
 
-            JsonObject movieJsonObj = (JsonObject) jsonParser.parse(movieInfo);
-            JsonObject movieDataJsonArr = (JsonObject)movieJsonObj.getAsJsonArray("Data").get(0);
-            JsonObject finalData = movieDataJsonArr.getAsJsonArray("Result").get(0).getAsJsonObject();
+            String kmdbResponse = OkHttpUtils.get(KMDB_URL + KMDB_API_KEY + "&title=" + movieNm.getAsString());
+            JsonObject kmdbRespToJson = (JsonObject) jsonParser.parse(kmdbResponse);
+            JsonObject kmdbData = (JsonObject) kmdbRespToJson.getAsJsonArray("Data").get(0);
+            JsonObject kmdbResult = kmdbData.getAsJsonArray("Result").get(0).getAsJsonObject();
+            JsonElement posters = kmdbResult.get("posters");
+            JsonElement prodYear = kmdbResult.get("prodYear");
+            JsonElement directors = kmdbResult.get("directors");
+            JsonElement nation = kmdbResult.get("nation");
+            JsonElement runtime = kmdbResult.get("runtime");
+            JsonElement rating = kmdbResult.get("rating");
+            JsonElement genre = kmdbResult.get("genre");
+            JsonElement actors = kmdbResult.get("actors");
+            JsonElement plots = kmdbResult.get("plots");
 
-            finalData2.add("title", finalData.get("title"));
-            finalData2.add("titleEng", finalData.get("titleEng"));
-            finalData2.add("prodYear", finalData.get("prodYear"));
-            finalData2.add("nation", finalData.get("nation"));
-            finalData2.add("runtime", finalData.get("runtime"));
-            finalData2.add("genre", finalData.get("genre"));
-            finalData2.add("rating", finalData.get("rating"));
-            finalData2.add("posters", finalData.get("posters"));
-            finalData2.add("directors", finalData.get("directors"));
-            finalData2.add("plots", finalData.get("plots"));
-            finalData2.add("actors", finalData.get("actors"));
+            movieObject.add("movieNm", movieNm);    // 제목
+            movieObject.add("audiAcc", audiAcc);    // 누적관객수
+            movieObject.add("openDt", openDt);      // 개봉일
+            movieObject.add("rank", rank);          // 랭킹
+            movieObject.add("posters", posters);    // 포스터 url
+            movieObject.add("prodYear", prodYear);
+            movieObject.add("directors", directors);
+            movieObject.add("nation", nation);
+            movieObject.add("runtime", runtime);
+            movieObject.add("rating", rating);
+            movieObject.add("genre", genre);
+            movieObject.add("actors", actors);
+            movieObject.add("plots", plots);
 
-            log.info(finalData);
+            log.info(movieObject);
         }
 
         model.addAttribute("loginMember", member);
