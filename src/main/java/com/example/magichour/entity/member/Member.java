04c963cd9777ad815598ff527b91cc2ac6f52bdf
Kim Chanho <beroso20th@gmail.com>
2023-11-23 02:37:24 +0900

    refactor : jwt관련 엔티티 수정

--- src/main/java/com/example/magichour/entity/member/Member.java
+++ src/main/java/com/example/magichour/entity/member/Member.java
@@ -4,7 +4,10 @@ import com.example.magichour.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Set;
+
 @Entity
+@Table(name = "member")
 @Getter
 @Builder
 @AllArgsConstructor
@@ -16,12 +19,22 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(length = 50, nullable = false)
+    @Column(name = "user_id", length = 50, nullable = false)
     private String userId;
 
-    @Column(length = 30, nullable = false)
+    @Column(name = "user_password", length = 30, nullable = false)
     private String userPassword;
 
-    @Column(length = 30, nullable = false)
+    @Column(name = "user_name", length = 30, nullable = false)
     private String userName;
+
+    @Column(name = "activated")
+    private boolean activated;
+
+    @ManyToMany
+    @JoinTable(
+            name = "member_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
 }
