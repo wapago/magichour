dcc425b2c88354d24e417a10c912cc78c003ace4
Kim Chanho <beroso20th@gmail.com>
2024-08-06 08:45:01 +0900

    Refactor: userId->userEmail

--- src/main/java/com/example/magichour/dto/member/LoginRequest.java
+++ src/main/java/com/example/magichour/dto/member/LoginRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class LoginRequest {
     @NotBlank(message = "아이디를 입력하세요")
-    private String userId;
+    private String userEmail;
 
     @NotBlank(message = "비밀번호를 입력하세요")
     private String userPassword;
