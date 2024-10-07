cda6d79d1e7e4eef11ff993b3192644d2296045a
Kim Chanho <beroso20th@gmail.com>
2024-10-07 16:23:54 +0900

    feat: 메타DB, 일반DB config 추가

---
+++ src/main/java/com/example/magichour/config/DataBaseConfig.java
@@ -0,0 +1,53 @@
+package com.example.magichour.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = {"com.example.magichour.repository"},
+        entityManagerFactoryRef = "dataEntityManager",
+        transactionManagerRef = "dataTransactionManager"
+)
+public class DataBaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-data")
+    public DataSource dataBaseSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean dataEntityManager() {
+        LocalContainerEntityManagerFactoryBean emBean = new LocalContainerEntityManagerFactoryBean();
+
+        emBean.setDataSource(dataBaseSource());
+        emBean.setPackagesToScan(new String[]{"com.example.magichour.entity.member"});
+        emBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", true);
+        emBean.setJpaPropertyMap(properties);
+
+        return emBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager dataTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(dataEntityManager().getObject());
+
+        return transactionManager;
+    }
+}
