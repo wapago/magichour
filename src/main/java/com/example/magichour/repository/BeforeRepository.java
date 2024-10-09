5e6bf66c80733fdcd27f7da1a44fb7a463f9d390
Kim Chanho <beroso20th@gmail.com>
2024-10-09 19:14:17 +0900

    chore: 불필요파일삭제

--- src/main/java/com/example/magichour/repository/BeforeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.magichour.repository;
-
-import com.example.magichour.entity.BeforeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
-}
