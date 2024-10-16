bfd5d3caddbb0f3c84ef3e47e790842778bbc792
Kim Chanho <beroso20th@gmail.com>
2024-10-16 18:50:57 +0900

    refactor: MovieEntity Id타입 수정

--- src/main/java/com/example/magichour/repository/MovieRepository.java
+++ src/main/java/com/example/magichour/repository/MovieRepository.java
@@ -3,5 +3,5 @@ package com.example.magichour.repository;
 import com.example.magichour.entity.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
+public interface MovieRepository extends JpaRepository<MovieEntity, String> {
 }
