4eaf5b31891e3664c04641ef9e4655bd99ab3d40
Kim Chanho <beroso20th@gmail.com>
2024-10-16 18:49:50 +0900

    refactor: docId->movieId / 리턴타입 / 엔드포인트

--- src/main/java/com/example/magichour/controller/movie/MovieController.java
+++ src/main/java/com/example/magichour/controller/movie/MovieController.java
@@ -1,12 +1,13 @@
 package com.example.magichour.controller.movie;
 
 import com.example.magichour.dto.movie.Movie;
+import com.example.magichour.entity.MovieEntity;
 import com.example.magichour.service.movie.MovieService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -16,21 +17,22 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Log4j2
+@RequestMapping("/api/magichour")
+@Slf4j
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping(value = "/home")
+    @GetMapping(value = "/")
     public ResponseEntity<List<Movie>> boxofficeList(HttpSession session) throws JsonProcessingException {
         List<Movie> boxofficeList = movieService.getDailyBoxofficeList();
 
         return ResponseEntity.ok(boxofficeList);
     }
 
-    @GetMapping(value = "/book/{docId}")
-    public ResponseEntity<Movie> movieDetail(HttpSession session, @PathVariable String docId) throws JsonProcessingException {
-        Movie movie = movieService.getMovie(docId);
+    @GetMapping(value = "/detail/{movieId}")
+    public ResponseEntity<MovieEntity> movieDetail(HttpSession session, @PathVariable String docId) {
+        MovieEntity movie = movieService.getMovie(docId);
 
         return ResponseEntity.ok(movie);
     }
