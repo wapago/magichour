2940d185b2a1a4f7c99ac54805ce9c3f70b09dcf
Kim Chanho <beroso20th@gmail.com>
2024-12-17 22:24:42 +0900

    refactor: winbatch삭제

--- src/main/java/com/example/magichour/controller/BatchController.java
+++ src/main/java/com/example/magichour/controller/BatchController.java
@@ -7,7 +7,6 @@ import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
@@ -23,19 +22,6 @@ public class BatchController {
     public BatchController(JobLauncher jobLauncher, JobRegistry jobRegistry) {
         this.jobLauncher = jobLauncher;
         this.jobRegistry = jobRegistry;
-    }
-
-    @GetMapping("/win")
-    public String winApi(@RequestParam("value") String value) throws Exception {
-
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addString("date", value)
-                .toJobParameters();
-
-        jobLauncher.run(jobRegistry.getJob("winbatchjob"), jobParameters);
-
-
-        return "ok";
     }
 
     @GetMapping("/kmdbcsv")
